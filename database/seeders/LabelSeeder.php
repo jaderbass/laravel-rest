37b0f1053a594827847c2c5f5d9a8292cc9940ac
Jörg Aderhold <joerg@jaderbass.de>
2025-04-23 13:46:00 +0200

    create Seeders

---
+++ database/seeders/LabelSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Label;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LabelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Label::factory()->count(5)->create();
+    }
+}
